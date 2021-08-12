bdbd872417899341a3732a9944ebbec7ce98cf05
Chloé R <clositi@live.fr>
2021-08-12 15:17:16 +0200

    added video in PhotographerMedia

--- js/components/PhotographerMedia.js
+++ js/components/PhotographerMedia.js
@@ -3,20 +3,35 @@ export default class PhotographerMedia {
 		this.title = data.title;
 		this.date = data.date;
 		this.image = data.image;
+		this.video = data.video;
 		this.likes = data.likes;
 		this.alt = data.alt;
 		this.photographerId = data.photographerId;
 	}
 
 	render() {
-		return `<article class="picture">
-                <div class="picture__img">
-                  <img src="img/${this.photographerId}/${this.image}" alt="${this.alt}">
+		if (this.video == null) {
+			return `<article class="media">
+                <div class="media__medium">
+                  <img class="media__img" src="img/${this.photographerId}/${this.image}" alt="${this.alt}">
                 </div>
-								<div class="picture__legend">
-									<h2 class="picture__title">${this.title}</h2>
-									<div class="picture__likes">${this.likes}</div>
+								<div class="media__legend">
+									<h2 class="media__title">${this.title}</h2>
+									<div class="media__likes">${this.likes}</div>
 								</div>
         </article>`;
+		} else {
+			return `<article class="media">
+							<div class="media__medium">
+								<video class="media__video" controls="controls" src="img/${this.photographerId}/${this.video}">
+									<p> ${this.alt} </p>
+								</video>
+							</div>
+							<div class="media__legend">
+								<h2 class="media__title">${this.title}</h2>
+								<div class="media__likes">${this.likes}</div>
+							</div>
+				</article>`;
+		}
 	}
 }
